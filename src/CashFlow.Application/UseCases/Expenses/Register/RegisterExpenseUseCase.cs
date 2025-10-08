5d0c7423dc5be8a99745dbd603be0ad27aec4e10
William de Lima <williamdelimacx@gmail.com>
2025-10-08 14:26:41 -0300

    feat: using linq to get only error messages as a list of strings

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -16,5 +16,12 @@ public class RegisterExpenseUseCase
         var validator = new RegisterExpenseValidator();
 
         var result = validator.Validate(request);
+
+        if (result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+            throw new ArgumentException();
+        }
     }
 }
