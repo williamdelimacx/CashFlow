df5f8024fcae3853b13011cdc41e774a366fdd9b
William de Lima <williamdelimacx@gmail.com>
2025-10-29 11:47:35 -0300

    feat: adding usecase to register expenses as dependency injection

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -9,9 +9,10 @@ namespace CashFlow.Api.Controllers;
 public class ExpensesController : ControllerBase
 {
     [HttpPost]
-    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    public IActionResult Register(
+        [FromServices] IRegisterExpenseUseCase useCase, 
+        [FromBody] RequestRegisterExpenseJson request)
     {
-        var useCase = new RegisterExpenseUseCase();
 
         var response = useCase.Execute(request);
 
