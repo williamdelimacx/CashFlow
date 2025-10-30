fe953a24244db20eb8c145209762dcef06f73304
William de Lima <williamdelimacx@gmail.com>
2025-10-30 17:43:54 -0300

    feat: adding expenses info into the excel file

--- src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Domain.Reports;
+﻿using CashFlow.Domain.Enums;
+using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
 using ClosedXML.Excel;
 
@@ -30,10 +31,35 @@ public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUs
 
         InsertHeader(worksheet);
 
+        var raw = 2;
+        foreach(var expense in expenses)
+        {
+            worksheet.Cell($"A{raw}").Value = expense.Title;
+            worksheet.Cell($"B{raw}").Value = expense.Date;
+            worksheet.Cell($"C{raw}").Value = ConvertPaymentType(expense.PaymentType);
+            worksheet.Cell($"D{raw}").Value = expense.Amount;
+            worksheet.Cell($"E{raw}").Value = expense.Description;
+
+            raw++;
+        }
+
         var file = new MemoryStream();
         workbook.SaveAs(file);
 
         return file.ToArray();
+    }
+
+    private string ConvertPaymentType(PaymentType payment)
+    {
+        return payment switch
+        {
+            PaymentType.Cash => "Cash",
+            PaymentType.CreditCard => "Credit Card",
+            PaymentType.DebitCard => "Debit Card",
+            PaymentType.EletronicTransfer => "Eletronic Transfer",
+            _ => string.Empty
+
+        };
     }
 
     private void InsertHeader(IXLWorksheet worksheet)
