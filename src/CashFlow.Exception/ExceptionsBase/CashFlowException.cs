8ca2987b48ca3d1de933e34ba730e9e077427111
William de Lima <williamdelimacx@gmail.com>
2025-10-30 08:37:07 -0300

    feat: adding not found custom exception

--- src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
+++ src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
@@ -1,4 +1,8 @@
 ﻿namespace CashFlow.Exception.ExceptionsBase;
 public abstract class CashFlowException : SystemException
 {
+    protected CashFlowException(string message) : base(message)
+    {
+
+    }
 }
