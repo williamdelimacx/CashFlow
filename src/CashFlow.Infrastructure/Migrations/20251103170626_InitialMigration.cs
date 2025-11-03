3035f7e7064cc210efce76553c17c3f4f4bcbaba
William de Lima <williamdelimacx@gmail.com>
2025-11-03 17:20:17 -0300

    feat: saving user in database

--- src/CashFlow.Infrastructure/Migrations/20251103170626_InitialMigration.cs
+++ src/CashFlow.Infrastructure/Migrations/20251103170626_InitialMigration.cs
@@ -12,7 +12,7 @@ namespace CashFlow.Infrastructure.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "User",
+                name: "Users",
                 columns: table => new
                 {
                     Id = table.Column<long>(type: "bigint", nullable: false)
@@ -25,7 +25,7 @@ namespace CashFlow.Infrastructure.Migrations
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_User", x => x.Id);
+                    table.PrimaryKey("PK_Users", x => x.Id);
                 });
 
             migrationBuilder.CreateTable(
@@ -47,7 +47,7 @@ namespace CashFlow.Infrastructure.Migrations
                     table.ForeignKey(
                         name: "FK_Expenses_User_UserId",
                         column: x => x.UserId,
-                        principalTable: "User",
+                        principalTable: "Users",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
@@ -65,7 +65,7 @@ namespace CashFlow.Infrastructure.Migrations
                 name: "Expenses");
 
             migrationBuilder.DropTable(
-                name: "User");
+                name: "Users");
         }
     }
 }
