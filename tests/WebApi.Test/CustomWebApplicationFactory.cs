c5393bb4d6578269a29f5d0d0eb2af38f858256e
William de Lima <williamdelimacx@gmail.com>
2025-11-05 19:01:15 -0300

    feat: adding db in memory

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApi.Test;
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.UseEnvironment("Test");
+        builder.UseEnvironment("Test")
+            .ConfigureServices(services =>
+            {
+                var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+                services.AddDbContext<CashFlowDbContext>(config =>
+                {
+                    config.UseInMemoryDatabase("InMemoryDbForTesting");
+                    config.UseInternalServiceProvider(provider);
+                });
+            });
     }
 }
