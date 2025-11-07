0dde02e8e5ee6fcc2848e287f05d556e13f1d203
William de Lima <williamdelimacx@gmail.com>
2025-11-07 10:20:47 -0300

    feat: validation about logged user on create and update expense

--- src/CashFlow.Application/UseCases/Expenses/Update/UpdateExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Update/UpdateExpenseUseCase.cs
@@ -3,6 +3,7 @@ using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Domain.Services.LoggedUser;
 using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
 using System.ComponentModel.DataAnnotations;
@@ -13,19 +14,23 @@ public class UpdateExpenseUseCase : IUpdateExpenseUseCase
     private readonly IMapper _mapper;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IExpensesUpdateOnlyRepository _repository;
+    private readonly ILoggedUser _loggedUser;
 
-    public UpdateExpenseUseCase(IMapper mapper, IUnitOfWork unitOfWork, IExpensesUpdateOnlyRepository repository)
+    public UpdateExpenseUseCase(IMapper mapper, IUnitOfWork unitOfWork, IExpensesUpdateOnlyRepository repository, ILoggedUser loggedUser)
     {
         _mapper = mapper;
         _unitOfWork = unitOfWork;
         _repository = repository;
+        _loggedUser = loggedUser;
     }
 
     public async Task Execute(long id, RequestExpenseJson request)
     {
         Validate(request);
 
-        var expense = await _repository.GetById(id);
+        var loggedUser = await _loggedUser.Get();
+
+        var expense = await _repository.GetById(loggedUser, id);
 
         if (expense is null)
         {
