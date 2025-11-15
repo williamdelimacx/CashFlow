1229771e82f968d5b15a926654eb9f0dc40b6882
William de Lima <williamdelimacx@gmail.com>
2025-11-15 17:01:33 -0300

    tests: tests for get all expenses

--- tests/WebApi.Test/CashFlowClassFixture.cs
+++ tests/WebApi.Test/CashFlowClassFixture.cs
@@ -26,6 +26,17 @@ public class CashFlowClassFixture : IClassFixture<CustomWebApplicationFactory>
         return await _httpClient.PostAsJsonAsync(requestUri, request);
     }
 
+    protected async Task<HttpResponseMessage> DoGet(
+        string requestUri,
+        string token,
+        string culture = "en")
+    {
+        AuthorizeRequest(token);
+        ChangeRequestCulture(culture);
+
+        return await _httpClient.GetAsync(requestUri);
+    }
+
     private void AuthorizeRequest(string token)
     {
         if (string.IsNullOrWhiteSpace(token))
