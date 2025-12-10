6dfb25f066ebedfb581fe49b11c941d5095d5e48
William de Lima <williamdelimacx@gmail.com>
2025-12-10 18:47:41 -0300

    feat: adding get user profile

---
+++ src/CashFlow.Application/UseCases/Users/Profile/IGetUserProfileUseCase.cs
@@ -0,0 +1,8 @@
+﻿using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Users.Profile;
+
+public interface IGetUserProfileUseCase
+{
+    Task<ResponseUserProfileJson> Execute();
+}
