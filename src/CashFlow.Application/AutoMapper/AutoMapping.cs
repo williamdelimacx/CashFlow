63850fccf21d16814655bee962ac887f69ef54ce
William de Lima <williamdelimacx@gmail.com>
2025-12-15 14:31:40 -0300

    feat: register an expense with tags

--- src/CashFlow.Application/AutoMapper/AutoMapping.cs
+++ src/CashFlow.Application/AutoMapper/AutoMapping.cs
@@ -4,6 +4,7 @@ using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
 
 namespace CashFlow.Application.AutoMapper;
+
 public class AutoMapping : Profile
 {
     public AutoMapping()
@@ -14,9 +15,15 @@ public class AutoMapping : Profile
 
     private void RequestToEntity()
     {
-        CreateMap<RequestExpenseJson, Expense>();
+
         CreateMap<RequestRegisterUserJson, User>()
             .ForMember(dest => dest.Password, config => config.Ignore());
+
+        CreateMap<RequestExpenseJson, Expense>()
+            .ForMember(dest => dest.Tags, config => config.MapFrom(source => source.Tags.Distinct()));
+
+        CreateMap<Communication.Enums.Tag, Tag>()
+            .ForMember(dest => dest.Value, config => config.MapFrom(source => source));
     }
 
     private void EntityToResponse()
