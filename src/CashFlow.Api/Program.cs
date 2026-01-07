246b88f6017ecaf3a611f8b1f83b604cd53ba1f5
William de Lima <williamdelimacx@gmail.com>
2026-01-07 10:44:27 -0300

    feat: adding healthcheck

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -4,9 +4,12 @@ using CashFlow.Api.Token;
 using CashFlow.Application;
 using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure;
+using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.Extensions;
 using CashFlow.Infrastructure.Migrations;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
@@ -73,7 +76,19 @@ builder.Services.AddAuthentication(config =>
     };
 });
 
+builder.Services.AddHealthChecks().AddDbContextCheck<CashFlowDbContext>();
+
 var app = builder.Build();
+
+app.MapHealthChecks("/Health", new HealthCheckOptions
+{
+    AllowCachingResponses = false,
+    ResultStatusCodes =
+    {
+        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable,
+    }
+});
 
 if (app.Environment.IsDevelopment())
 {
@@ -103,5 +118,3 @@ async Task MigrateDatabase()
 
     await DataBaseMigration.MigrateDatabase(scope.ServiceProvider);
 }
-
-public partial class Program { }
--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using WebApi.Test.Resources;
 
 namespace WebApi.Test;
